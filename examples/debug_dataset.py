323902fb74217584daefb70d28ab3f2176f40d5e
Pavel Kral <pavel@pavelkral.eu>
2019-03-16 16:40:31 +0100

    new methods in examples

--- examples/debug_dataset.py
+++ examples/debug_dataset.py
@@ -2,7 +2,7 @@
 from graffiti_dataset.dataset import DatasetSample
 import cv2
 import glob
-from graffiti_dataset.tools import draw_main_colors
+from graffiti_dataset.tools import draw_main_colors, draw_color_cube, draw_hsv_pixels
 
 for sample in glob.glob('./dataset/graffiti_sample/*.p'):
 
@@ -16,9 +16,13 @@ for sample in glob.glob('./dataset/graffiti_sample/*.p'):
     cv2.imwrite(f'./readme_images/{sample.sample_id}_background_mask.png', sample.background_mask)
     cv2.imwrite(f'./readme_images/{sample.sample_id}_incomplete_graffiti_mask.png', sample.incomplete_graffiti_mask)
 
-    sample_main_colors = sample.main_colors()
+    sample_main_colors = sample._kmeans_color_clusters()
 
-    draw_main_colors(sample_main_colors, f'./readme_images/{sample.sample_id}_colors.png')
+    draw_main_colors(sample_main_colors, f'./readme_images/{sample.sample_id}_colors_rgb.png')
+
+    sample_main_colors = sample._hsv_color_clusters_dbscan()
+
+    draw_main_colors(sample_main_colors, f'./readme_images/{sample.sample_id}_colors_hsv.png')
 
     sample.random_rotate()
 
@@ -29,4 +33,10 @@ for sample in glob.glob('./dataset/graffiti_sample/*.p'):
 
     cv2.imwrite(f'./readme_images/{sample.sample_id}_elastic_image.png', sample.image)
     cv2.imwrite(f'./readme_images/{sample.sample_id}_elastic_mask.png', sample.graffiti_mask)
+
+    sample_pixels = sample.graffiti_pixels(10)
+
+    draw_color_cube(sample_pixels, f'./readme_images/{sample.sample_id}_pixel_cube.html')
+
+    draw_hsv_pixels(sample, f'./readme_images/{sample.sample_id}_hue_hsv_pixels.png')
 
