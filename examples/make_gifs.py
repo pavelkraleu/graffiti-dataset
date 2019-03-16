3aaba66fd7c7cedcd688221179b7f13ae47acff3
Pavel Kral <pavel@pavelkral.eu>
2019-03-16 17:03:44 +0100

    new gifs

--- examples/make_gifs.py
+++ examples/make_gifs.py
@@ -4,13 +4,24 @@ from graffiti_dataset.dataset import DatasetSample
 import cv2
 import imageio
 from graffiti_dataset.tools import random_background
+from skimage.transform import rescale
+import numpy as np
 
 num_images = 10
 source_sample = './dataset/graffiti/7f7fff8f89837f00.p'
 # source_sample = './dataset/graffiti/070b070707cfa7ff.p'
 gif_duration = 0.5
-
+rescale_ratio = 0.25
 images = []
+
+def resize_image(image):
+    return rescale(
+        image,
+        rescale_ratio,
+        anti_aliasing=True,
+        multichannel=True,
+        preserve_range=True,
+        mode='reflect').astype(np.uint8)
 
 for _ in range(num_images):
 
@@ -21,7 +32,15 @@ for _ in range(num_images):
     sample.randomize_color()
     # sample.elastic_transform()
 
-    images.append(cv2.cvtColor(sample.image,cv2.COLOR_BGR2RGB))
+    # images.append(
+    #     rescale(
+    #         cv2.cvtColor(sample.image,cv2.COLOR_BGR2RGB),
+    #         rescale_ratio,
+    #         anti_aliasing=True,
+    #         multichannel=True, preserve_range=True, mode='reflect').astype(np.uint8)
+    #     )
+
+    images.append(resize_image(cv2.cvtColor(sample.image,cv2.COLOR_BGR2RGB)))
 
 imageio.mimsave('./readme_images/random_colors.gif', images, duration=gif_duration)
 
@@ -36,7 +55,8 @@ for _ in range(num_images):
     sample.randomize_color()
     sample.elastic_transform()
 
-    images.append(cv2.cvtColor(sample.image,cv2.COLOR_BGR2RGB))
+    # images.append(cv2.cvtColor(sample.image,cv2.COLOR_BGR2RGB))
+    images.append(resize_image(cv2.cvtColor(sample.image, cv2.COLOR_BGR2RGB)))
 
 imageio.mimsave('./readme_images/random_colors_elastic.gif', images, duration=gif_duration)
 
@@ -50,7 +70,8 @@ for _ in range(num_images):
 
     sample.elastic_transform()
 
-    images.append(cv2.cvtColor(sample.image,cv2.COLOR_BGR2RGB))
+    # images.append(cv2.cvtColor(sample.image,cv2.COLOR_BGR2RGB))
+    images.append(resize_image(cv2.cvtColor(sample.image, cv2.COLOR_BGR2RGB)))
 
 imageio.mimsave('./readme_images/elastic.gif', images, duration=gif_duration)
 
@@ -68,7 +89,8 @@ for _ in range(num_images):
 
     sample.paste_on_background(background_image)
 
-    images.append(cv2.cvtColor(sample.image,cv2.COLOR_BGR2RGB))
+    # images.append(cv2.cvtColor(sample.image,cv2.COLOR_BGR2RGB))
+    images.append(resize_image(cv2.cvtColor(sample.image, cv2.COLOR_BGR2RGB)))
 
 imageio.mimsave('./readme_images/random_background.gif', images, duration=gif_duration)
 
@@ -88,7 +110,8 @@ for _ in range(num_images):
 
     sample.paste_on_background(background_image)
 
-    images.append(cv2.cvtColor(sample.image,cv2.COLOR_BGR2RGB))
+    # images.append(cv2.cvtColor(sample.image,cv2.COLOR_BGR2RGB))
+    images.append(resize_image(cv2.cvtColor(sample.image, cv2.COLOR_BGR2RGB)))
 
 imageio.mimsave('./readme_images/random_background_colors.gif', images, duration=gif_duration)
 
@@ -109,8 +132,8 @@ for _ in range(num_images):
 
     sample.paste_on_background(background_image)
 
-    images.append(cv2.cvtColor(sample.image,cv2.COLOR_BGR2RGB))
-
+    # images.append(cv2.cvtColor(sample.image,cv2.COLOR_BGR2RGB))
+    images.append(resize_image(cv2.cvtColor(sample.image, cv2.COLOR_BGR2RGB)))
 
 imageio.mimsave('./readme_images/random_background_colors_all.gif', images, duration=gif_duration)
 
